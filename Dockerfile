2e3b995051292fad0acc0e2edcf9692f8c19fc44
Ex-Geek <ex-geek@tuta.io>
2023-05-24 22:14:26 +0330

    v0.0.1 - working version

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM ubuntu:bionic
 RUN apt-get update && apt-get install -y curl wget unzip
 RUN curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash
 RUN apt-get install -y speedtest
-#RUN mkdir -p /dev/net && mknod /dev/net/tun c 10 200 && chmod 600 /dev/net/tun
 RUN apt-get install -y uml-utilities
 RUN wget -q https://github.com/Jigsaw-Code/outline-go-tun2socks/releases/download/v3.2.0/linux.zip
 RUN unzip linux.zip
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-tunctl -t outline-tap0
 echo "-----------SPEEDTEST WITHOUT OUTLINE - IRAN (PISHGAMAN)-----------"
-echo "yes" | speedtest -s 32500 -f json
+speedtest -s 32500 -f json --accept-license
 echo "-----------SPEEDTEST WITHOUT OUTLINE - GERMANY (RETN)-----------"
 speedtest -s 31120 -f json
+ip tuntap add dev outline-tap0 mode tun
 ip link set dev outline-tap0 up
-ip addr add 10.0.0.1/24 dev tun0
-ip -4 route add 0.0.0.0/0 via 10.0.85.1 dev outline-tap0 onlink
-./tun2socks -tunName outline-tap0 -tunDNS 1.1.1.1,9.9.9.9 -tunAddr 10.0.85.2 -tunGw 10.0.85.1 -tunMask 255.255.255.0 -proxyHost $OUTLINE_IP -proxyPort $OUTLINE_PORT -proxyPassword $OUTLINE_PASS -proxyCipher $OUTLINE_CIPHER -proxyPrefix $OUTLINE_PREFIX &
+read IP_ROUTE <<< $(ip route | awk '/default/{ $1=""; print $0 }')
+ip route add $OUTLINE_IP/32 $IP_ROUTE metric 0
+ip route del default
+ip addr add 10.0.85.2/24 dev outline-tap0
+ip route add default via 10.0.85.1 onlink dev outline-tap0 metric 0
+./tun2socks -tunName outline-tap0 -proxyHost $OUTLINE_IP -proxyPort $OUTLINE_PORT -proxyPassword $OUTLINE_PASS -proxyCipher $OUTLINE_CIPHER -proxyPrefix $OUTLINE_PREFIX &
 echo "-----------SPEEDTEST WITH OUTLINE - GERMANY (RETN)-----------"
 speedtest -s 31120 -f json
-killall tun2socks
-killall tun2socks
