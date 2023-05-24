6241a6ecec98549fa9f269268c68085094cb69b2
Ex-Geek <ex-geek@tuta.io>
2023-05-24 22:58:36 +0330

    v0.0.1 - replaced default nameservers

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+printf 'nameserver\t1.1.1.1\nnameserver\t4.2.2.4\n' > /etc/resolv.conf
 echo "-----------SPEEDTEST WITHOUT OUTLINE - IRAN (PISHGAMAN)-----------"
-speedtest -s 32500 -f json --accept-license
+speedtest -s 32500 -f json --accept-license --accept-gdpr
 echo "-----------SPEEDTEST WITHOUT OUTLINE - GERMANY (RETN)-----------"
 speedtest -s 31120 -f json
 ip tuntap add dev outline-tap0 mode tun
@@ -11,6 +12,6 @@ ip route add $OUTLINE_IP/32 $IP_ROUTE metric 0
 ip route del default
 ip addr add 10.0.85.2/24 dev outline-tap0
 ip route add default via 10.0.85.1 onlink dev outline-tap0 metric 0
-./tun2socks -tunName outline-tap0 -proxyHost $OUTLINE_IP -proxyPort $OUTLINE_PORT -proxyPassword $OUTLINE_PASS -proxyCipher $OUTLINE_CIPHER -proxyPrefix $OUTLINE_PREFIX &
+./tun2socks -tunName outline-tap0 -proxyHost $OUTLINE_IP -proxyPort $OUTLINE_PORT -proxyPassword $OUTLINE_PASS -proxyCipher $OUTLINE_CIPHER -proxyPrefix $OUTLINE_PREFIX -dnsFallback &
 echo "-----------SPEEDTEST WITH OUTLINE - GERMANY (RETN)-----------"
 speedtest -s 31120 -f json
