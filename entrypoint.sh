b82d5ac13f03fa1b3e6ed50a09ef7e05607bc6e8
Ex-Geek <ex-geek@tuta.io>
2023-05-24 23:45:12 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "-----------SPEEDTEST WITHOUT OUTLINE - IRAN (PISHGAMAN)-----------"
 speedtest -s 32500 -f json --accept-license --accept-gdpr
 echo "-----------SPEEDTEST WITHOUT OUTLINE - GERMANY (RETN)-----------"
-speedtest -s 31120 -f json
+speedtest -s 31120 -f json --accept-license --accept-gdpr
 ip tuntap add dev outline-tap0 mode tun
 ip link set dev outline-tap0 up
 read IP_ROUTE <<< $(ip route | awk '/default/{ $1=""; print $0 }')
@@ -13,4 +13,4 @@ ip addr add 10.0.85.2/24 dev outline-tap0
 ip route add default via 10.0.85.1 onlink dev outline-tap0 metric 0
 ./tun2socks -tunName outline-tap0 -proxyHost $OUTLINE_IP -proxyPort $OUTLINE_PORT -proxyPassword $OUTLINE_PASS -proxyCipher $OUTLINE_CIPHER -proxyPrefix $OUTLINE_PREFIX -dnsFallback -logLevel debug &
 echo "-----------SPEEDTEST WITH OUTLINE - GERMANY (RETN)-----------"
-speedtest -s 31120 -f jsonl
+speedtest -s 31120 -f jsonl --accept-license --accept-gdpr
